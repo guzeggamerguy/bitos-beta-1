449d53a71c02b1946c79522271625d094339db54
guzeggamerguy <72354795+guzeggamerguy@users.noreply.github.com>
2022-06-02 14:06:28 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,79 @@
+basic.showNumber(0.1)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.clearScreen()
+basic.showString("BitOS beta 1")
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . . . # .
+    . . # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.clearScreen()
+basic.showString("NO OPERATING SYSTEM DETECTED")
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . . . # .
+    . . # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.clearScreen()
+basic.showString("Starting MBPOS")
+basic.showString("@\\")
+basic.showString("@\\(A/B)")
